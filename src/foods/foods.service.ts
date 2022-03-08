52fe1100f3fe706c55bc77797a5101fcff3eec56
asdf148 <wnduf0405_@naver.com>
2022-03-08 19:58:48 +0900

    [FEAT] Implment getFoods founction

--- src/foods/foods.service.ts
+++ src/foods/foods.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { FoodRepository } from './entity/food.repository';
 
 @Injectable()
-export class FoodsService {}
+export class FoodsService {
+  constructor(private readonly foodRepository: FoodRepository) {}
+
+  getFoods(): Promise<any> {
+    return this.foodRepository.findAll();
+  }
+}
